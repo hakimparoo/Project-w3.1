22d7d13ecbb5abcc51789f440006c0a7ed1450ff
hakimparoo <69505770+hakimparoo@users.noreply.github.com>
2020-08-30 23:09:20 +0700

    Add project files.

---
+++ Project w3/Source.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+int main() {
+	int x;
+	scanf_s("%d", &x);
+	for (int i = 0;i <= x;i++) {
+		for (int j = 1;j <= i;j++) {
+			printf_s(" ");
+		}
+		for (int j = 1;j <= x - i;j++) {
+			printf_s("*");
+		}
+		for (int j = 0;j <= x - i;j++) {
+			printf_s("*");
+		}
+		for (int j = 1;j <= i;j++) {
+			printf_s(" ");
+		}
+		printf_s("\n");
+
+	}
+	for (int i = 0;i <= x;i++) {
+		for (int j = 1;j <= x - i;j++) {
+			printf_s(" ");
+		}
+		for (int j = 1;j <= i;j++) {
+			printf_s("*");
+		}
+		for (int j = 0;j <= i;j++) {
+			printf_s("*");
+		}
+		for (int j = 1;j <= x - i;j++) {
+			printf_s(" ");
+		}
+		printf_s("\n");
+
+	}
+
+}
